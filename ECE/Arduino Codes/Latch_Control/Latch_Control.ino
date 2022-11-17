ac22a61664463dd6a7f9e6285f4b05c2cdc57512
JoelJojoP <joeljojop@gmail.com>
2022-11-17 21:39:58 +0530

    Arduino Codes Updated

---
+++ ECE/Arduino Codes/Latch_Control/Latch_Control.ino
@@ -0,0 +1,33 @@
+//Program to control latch of shooting mechanism
+
+#include <Servo.h>
+Servo myservo;  
+int pos = 0;    // variable to store the servo position
+int buttonState=1;
+int buttonPin=11;
+int state=0;
+int prevState=1;
+long int t1;
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(buttonPin,INPUT_PULLUP);
+}
+
+void loop() {
+  buttonState=digitalRead(buttonPin);
+  if(buttonState){
+    prevState=1;
+  }
+  if(buttonState==LOW && buttonState!=prevState){
+    prevState=buttonState;
+    if(state==90){
+      myservo.write(0);
+      state=0;
+    }
+    else{
+      myservo.write(90);
+      state=90;
+    }
+    delay(500);
+  }
+}
