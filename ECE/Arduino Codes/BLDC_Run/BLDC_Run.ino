83023453c832cd65d9df2e900581c0fac2161498
JoelJojoP <joeljojop@gmail.com>
2023-04-14 01:28:10 +0530

    BLDC code for amanual bot updated

--- ECE/Arduino Codes/BLDC_Run/BLDC_Run.ino
+++ ECE/Arduino Codes/BLDC_Run/BLDC_Run.ino
@@ -7,29 +7,29 @@ int speed=0;
 void setup() {    
   ESC.attach(9,1000,2000);
   //Calibration
-  // ESC.write(180);
-  // delay(5000);
-  // ESC.write(0);
-  // delay(5000);
-  Serial.begin(115200);
-  pinMode(2,INPUT_PULLUP);
-  pinMode(3,INPUT_PULLUP);
+  ESC.write(180);
+  delay(5000);
+  ESC.write(0);
+  delay(5000);
+  // Serial.begin(115200);
+  // pinMode(2,INPUT_PULLUP);
+  // pinMode(3,INPUT_PULLUP);
   ESC.write(0);
 }
 
 void loop() {
-  if(digitalRead(2)==LOW && speed < 175)
+  if(speed <= 180)
   {
       speed+=5;
       ESC.write(speed);
       delay(100);
   }
 
-  if(digitalRead(3)==LOW && speed >5)
-  { 
-      speed-=5;
-      ESC.write(speed);
-      delay(100);
-  }
-  Serial.println(speed);
+  // if(digitalRead(3)==LOW && speed >5)
+  // { 
+  //     speed-=5;
+  //     ESC.write(speed);
+  //     delay(100);
+  // }
+  // Serial.println(speed);
 }
