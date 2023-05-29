c72ebac922eec2e342bb814a4e9e368f62e459a4
JoelJojoP <joeljojop@gmail.com>
2023-05-29 20:26:19 +0530

    Final code template for controller side nano done

--- ECE/Final Codes/ER_Cont_Nano/ER_Cont_Nano.ino
+++ ECE/Final Codes/ER_Cont_Nano/ER_Cont_Nano.ino
@@ -69,73 +69,111 @@ void loop() {
     msg = "rst_nano";
   else if (Rst_MPU)
     msg = "rst_MPU";
+  else if (digitalRead(adj_pos) == 0)
+    // Adjusting position command
+    msg = "adj";
+  else if (digitalRead(type1) == 0)
+    // Aim at type 1 pole
+    msg = "type1";
+  else if (digitalRead(type2) == 0)
+    // Aim at type 2 pole
+    msg = "type2";
+  else if (digitalRead(type3) == 0)
+    // Aim at type 3 pole
+    msg = "type3";
+  else if (digitalRead(type4) == 0)
+    // Aim at type 4 pole
+    msg = "type4";
+  else if (digitalRead(type5) == 0)
+    // Aim at type 5 pole
+    msg = "type5";
+  else if (digitalRead(rotate_cw) == 0)
+    // Rotate the robot clockwise 90 degrees
+    msg = "cw";
+  else if (digitalRead(rotate_ccw) == 0)
+    // Rotate the robot counter-clockwise 90 degrees
+    msg = "ccw";
   else {
-
-    // Hangling pick buttons
-    int pick;
-    if (digitalRead(plate_up) == 0)
-      pick = 1;
-    else if (digitalRead(plate_down) == 0)
-      pick = -1;
-    else
-      pick = 0;
-
-    // Handling lock buttons
-    if (digitalRead(lock_x) == 1 && lockx_prev == 0) {
-      lock_x = !lock_x;
-      lockx_prev = 1;
-    } else {
-      lockx_prev = 0;
-    }
-    if (digitalRead(lock_y) == 1 && locky_prev == 0) {
-      lock_y = !lock_y;
-      locky_prev = 1;
-    } else {
-      locky_prev = 0;
-    }
-
-    //Handling rest other buttons on PS2 controller
-    int LY = ps2x.Analog(PSS_LY);
-    int LX = ps2x.Analog(PSS_LX);
-    int RX = ps2x.Analog(PSS_RX);
-    int Speed = ps2x.Button(PSB_SQUARE);
     int Reload = ps2x.Button(PSB_L1);
     int Shoot = ps2x.Button(PSB_R1);
-    int Angle_Up = ps2x.Button(PSB_PAD_UP);
-    int Angle_Down = ps2x.Button(PSB_PAD_DOWN);
-    int Pull = ps2x.Button(PSB_PAD_LEFT);
-    int Push = ps2x.Button(PSB_PAD_RIGHT);
 
-    //Calculate velocities
-    float vel_y = ((128 - LY) * 100 / 128.0);  //- 3.91;            //Uncomment to remove offset if any
-    float vel_x = ((LX - 128) * 100 / 128.0);  //+ 3.91;
-    float vel_w = ((RX - 128) * 100 / 128.0);  //+ 3.91;
+    if (Reload)
+      msg = "rel";
+    else if (Shoot)
+      msg = "sht";
+    else {
+      //Handling rest other buttons on PS2 controller
+      int LY = ps2x.Analog(PSS_LY);
+      int LX = ps2x.Analog(PSS_LX);
+      int RX = ps2x.Analog(PSS_RX);
+      int Speed = ps2x.Button(PSB_SQUARE);
+      int Angle_Up = ps2x.Button(PSB_PAD_UP);
+      int Angle_Down = ps2x.Button(PSB_PAD_DOWN);
+      int Pull = ps2x.Button(PSB_PAD_LEFT);
+      int Push = ps2x.Button(PSB_PAD_RIGHT);
 
-    //Values to move stepper and tilt motors
-    int tilt, stretch;
-    if (Angle_Up)
-      tilt = 1;
-    else if (Angle_Down)
-      tilt = -1;
-    else
-      tilt = 0;
-    if (Pull)
-      stretch = 1;
-    else if (Push)
-      stretch = -1;
-    else
-      stretch = 0;
+      // Hangling pick buttons
+      int pick;
+      if (digitalRead(plate_up) == 0)
+        pick = 1;
+      else if (digitalRead(plate_down) == 0)
+        pick = -1;
+      else
+        pick = 0;
 
-    // Handling speed button
-    if (Speed == 1 && speed_prev == 0) {
-      speed = !speed;
-      speed_prev = 1;
-    } else {
-      speed_prev = 0;
+      // Handling lock buttons
+      if (digitalRead(lock_x) == 1 && lockx_prev == 0) {
+        lockx = !lockx;
+        lockx_prev = 1;
+      } else {
+        lockx_prev = 0;
+      }
+      if (digitalRead(lock_y) == 1 && locky_prev == 0) {
+        locky = !locky;
+        locky_prev = 1;
+      } else {
+        locky_prev = 0;
+      }
+
+      //Calculate velocities
+      float vel_y = ((128 - LY) * 100 / 128.0);  //- 3.91;            //Uncomment to remove offset if any
+      float vel_x = ((LX - 128) * 100 / 128.0);  //+ 3.91;
+      float vel_w = ((RX - 128) * 100 / 128.0);  //+ 3.91;
+
+      //Values to move stepper and tilt motors
+      int tilt, stretch;
+      if (Angle_Up)
+        tilt = 1;
+      else if (Angle_Down)
+        tilt = -1;
+      else
+        tilt = 0;
+      if (Pull)
+        stretch = 1;
+      else if (Push)
+        stretch = -1;
+      else
+        stretch = 0;
+
+      // Handling speed button
+      if (Speed == 1 && speed_prev == 0) {
+        speed = !speed;
+        speed_prev = 1;
+      } else {
+        speed_prev = 0;
+      }
+
+      // Turn on indicators accordingly
+      if (speed)
+        digitalWrite(speed_in, HIGH);
+      if (lockx)
+        digitalWrite(lockx_in, HIGH);
+      if (locky)
+        digitalWrite(locky_in, HIGH);
+
+      // val <velx> <vely> <velw> <pick> <stretch> <tilt> <lockx> <locky> <speed>
+      msg = String("val") + String(" ") + String(vel_y) + String(" ") + String(vel_x) + String(" ") + String(vel_w) + " " + String(pick) + " " + String(stretch) + String(" ") + String(tilt) + " " + String(lockx) + " " + String(locky) + " " + String(speed);
     }
-
-    // val <velx> <vely> <velw> <pick> <stretch> <tilt> <lockx> <locky> <speed>
-    msg = String("val") + String(" ") + String(vel_y) + String(" ") + String(vel_x) + String(" ") + String(vel_w) + " " + String(pick) + " " + String(stretch) + String(" ") + String(tilt) + " " + String(lockx) + " " + String(locky) + " " + String(speed);
   }
 
   // Send this message to ESP07
