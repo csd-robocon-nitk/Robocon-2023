9346e2f50a099a5fad18d71fbd84252096ea9416
JoelJojoP <joeljojop@gmail.com>
2023-05-30 23:36:06 +0530

    Added template code for rr side nano

---
+++ ECE/Final Codes/RR_Bot_Nano/RR_Bot_Nano.ino
@@ -0,0 +1,187 @@
+#include <Servo.h>
+
+#define LF 0x0A
+
+// Change these
+#define gantry_right 0
+#define gantry_left 0
+#define shooter_back 0
+#define shooter_front 0
+#define pick_up 0
+#define pick_down 0
+#define esc_pin 0
+#define servo_pin 0
+#define push_pwm 0
+#define push_dir 0
+#define pick_pwm 0
+#define pick_dir 0
+#define gantry_pwm 0
+#define gantry_dir 0
+#define tilt_pwm 0
+#define tilt_dir 0
+
+Servo g_servo, esc;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(gantry_right, INPUT_PULLUP);
+  pinMode(gantry_left, INPUT_PULLUP);
+  pinMode(shooter_back, INPUT_PULLUP);
+  pinMode(shooter_front, INPUT_PULLUP);
+  pinMode(pick_up, INPUT_PULLUP);
+  pinMode(pick_down, INPUT_PULLUP);
+  pinMode(pick_dir, OUTPUT);
+  pinMode(pick_pwm, OUTPUT);
+  pinMode(gantry_dir, OUTPUT);
+  pinMode(gantry_pwm, OUTPUT);
+  pinMode(tilt_pwm, OUTPUT);
+  pinMode(tilt_dir, OUTPUT);
+  pinMode(push_pwm, OUTPUT);
+  pinMode(push_dir, OUTPUT);
+  g_servo.attach(servo_pin);
+  esc.attach(esc_pin, 1000, 2000);
+  // Calibrating ESC
+  esc.write(180);
+  delay(5000);
+  esc.write(0);
+  delay(5000);
+  shoot_seq();
+}
+
+// For serial communication
+char msg_str[100];
+char str_buff[7];
+int idx;
+
+// For picking
+int pick = 0;
+
+// For tilting
+int tilt = 0;
+
+// Function to move picking plate
+void move_picker() {
+  if (pick == 1 && analogRead(pick_up) != 0) {
+    digitalWrite(pick_dir, LOW);
+    analogWrite(pick_pwm, 100);
+  } else if (pick == -1 && analogRead(pick_down) != 0) {
+    digitalWrite(pick_dir, HIGH);
+    analogWrite(pick_pwm, 255);
+  } else {
+    analogWrite(pick_pwm, 0);
+  }
+}
+
+// Function to tilt shooting mechanism
+void move_tilt() {
+  if (tilt == 1) {
+    digitalWrite(tilt_dir, LOW);
+    digitalWrite(tilt_pwm, 1);
+  } else if (tilt == -1) {
+    digitalWrite(tilt_dir, HIGH);
+    digitalWrite(tilt_pwm, 1);
+  } else {
+    digitalWrite(tilt_pwm, 0);
+  }
+}
+
+// Function for reloading sequence
+void reload_seq() {
+  while (digitalRead(gantry_left) != 0) {
+    // Move gantry left
+    digitalWrite(gantry_dir, 1);
+    digitalWrite(gantry_pwm, 1);
+  }
+  digitalWrite(gantry_pwm, 0);
+  while (analogRead(pick_up) != 0) {
+    //Move picking plate up
+    digitalWrite(pick_dir, LOW);
+    analogWrite(pick_pwm, 100);
+  }
+  analogWrite(pick_pwm, 0);
+  while (analogRead(gantry_right) >= 5) {
+    //Move gantry right
+    digitalWrite(gantry_dir, 0);
+    digitalWrite(gantry_pwm, 1);
+  }
+  digitalWrite(gantry_pwm, 0);
+  //Move gantry servo
+  g_servo.write(170);
+  delay(500);
+  g_servo.write(0);
+  while (digitalRead(gantry_left) != 0) {
+    //Move gantry left
+    digitalWrite(gantry_dir, 1);
+    digitalWrite(gantry_pwm, 1);
+  }
+  digitalWrite(gantry_pwm, 0);
+}
+
+// Function to run shooting sequence
+void shoot_seq() {
+  // Turn on BLDC
+  esc.write(180);
+  delay(500);
+
+  // Push ring forward
+  while (digitalRead(shooter_front) != 0) {
+    digitalWrite(push_dir, LOW);
+    digitalWrite(push_pwm, HIGH);
+  }
+  digitalWrite(push_pwm, LOW);
+  while (digitalRead(shooter_back) != 0) {
+    digitalWrite(push_dir, HIGH);
+    digitalWrite(push_pwm, HIGH);
+  }
+  digitalWrite(push_pwm, LOW);
+
+  // Switch off BLDC
+  esc.write(0);
+}
+
+
+void loop() {
+  // Reading data from esp07
+  while (Serial.available()) {
+    msg_str[idx] = Serial.read();
+    if (msg_str[idx] == LF) {
+      msg_str[idx - 1] = 0;
+      idx = 0;
+      break;
+    }
+    idx++;
+  }
+
+  int i = 4;
+  int j = 0;
+  int var = 1;
+
+  // Splitting the data
+  if (msg_str[0] == 'v' && msg_str[1] == 'a' && msg_str[2] == 'l') {
+    while (var <= 10) {
+      if (msg_str[i] == ' ' || msg_str[i] == 0) {
+        str_buff[j] = 0;
+        switch (var) {
+          case 4:
+            pick = atoi(str_buff);
+            break;
+          case 6:
+            tilt = atoi(str_buff);
+            break;
+        }
+        var++;
+        j = -1;
+      } else {
+        str_buff[j] = msg_str[i];
+      }
+      i++;
+      j++;
+    }
+  } else if (msg_str[0] == 's' && msg_str[1] == 'h' && msg_str[2] == 't') {
+    shoot_seq();
+  } else if (msg_str[0] == 'r' && msg_str[1] == 'e' && msg_str[2] == 'l') {
+    reload_seq();
+  }
+  move_picker();
+  move_tilt();
+}
